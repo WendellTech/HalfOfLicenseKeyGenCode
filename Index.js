a5eb3340a61d7acd7691efddf2aec800ad3af069
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2023-08-29 21:49:31 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,4 +1,4 @@
-var licenseData = {info:StudentUser, prodCode:"EEN850392", appVersion:"1.0"}
+var licenseData = {info:StudentUser, prodCode:"", appVersion:"1.0"}
 
 }catch(err){
     console.log(err);
